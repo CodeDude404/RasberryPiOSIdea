74185d7c633566e41320afd306f31ddeda26f1c6
Liam Outlaw <68038925+CodeDude404@users.noreply.github.com>
2021-08-19 17:19:32 +0000

    Update Update System

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import os
-
+import threading
 from flask import Flask
 
 app = Flask(__name__)
 
+
+def update_system():
+	os.system("curl -o save.zip https://codeload.github.com/CodeDude404/RasberryPiOSIdea/zip/refs/heads/master")
+	os.system("unzip save.zip")
+	os.system("mv RasberryPiOSIdea-master/* ./")
+
 @app.route('/')
-def update():
-    return '<a href="system/update">open update thingy</a>'
+def index():
+	return '<a href="system/update">open update thingy</a>'
 
 
 @app.route('/system/update')
 def update():
-    return 'Updating System...'
-	os.system("curl https://github.com/CodeDude404/RasberryPiOSIdea/archive/refs/heads/master.zip >> output.zip")
+	print("updating")
+	# curl -o hello.zip ftp://speedtest.tele2.net/1MB.zip
+	update_system()
+	return 'Updating System...'
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+	app.run(debug=False, host='0.0.0.0')
